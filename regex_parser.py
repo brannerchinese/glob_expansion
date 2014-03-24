bf732a6273d27c33e9b7e1da9a90a0804423a2b6
dpb15 <brannerchinese@gmail.com>
2014-03-24 09:27:32 -0400

    add preparser to parser

--- regex_parser.py
+++ regex_parser.py
@@ -10,10 +10,23 @@ from collections import deque
 import inspect
 import pprint
 
+def preparse(s):
+    output = []
+    for c in s:
+        if c == ')':
+            return output
+        if c == '(':
+            output.append(preparse(s[1:]))
+        else:
+            output.append(c)
+
+
 class Sequence:
     def __init__(self, s):
         self.out_seq = []
         self.in_q = deque(s)
+        self.is_substring = False
+        self.c = ''
 
     def normalize(self):
         # n: normalized regex string; now list, later nested tuple
@@ -32,33 +45,36 @@ class Sequence:
                 }
         concatenate_me = ()
         while self.in_q:
-            c = self.in_q.popleft()
-            if c not in process:
-                self.out_seq.append(self.literal(c))
+            self.c = self.in_q.popleft()
+            print(self.c)
+            if self.c not in process:
+                self.out_seq.append(self.literal(self.c))
             else:
-                self.out_seq.append(process[n])
-        pprint.pprint('As list: {}'.format(self.out_seq))
+                self.out_seq.append(process[self.c]())
+            print(self.out_seq)
+#        pprint.pprint('As list: {}'.format(self.out_seq)) # debug
         return self.concatenate()
 
-    def open_substring(self, c):
-        # begin new run
-        substring_open = False
-        return (c, '(')
+    def open_substring(self):
+        """Begin recursive normalization of substring, to be stopped by ')'."""
+        # Haven't thought this out, but each () pair should be handled by
+        # stack.
+        substring = Sequence(self.in_q)
+        substring.is_substring = True
+        normalized = normalize(substring)
+        substring.is_substring = False
 
-    def close_substring(self, c):
-        # end run and process
-        # next substring concatenates
-        substring_open = False
-        return (c, ')')
+
+    def close_substring(self):
+        return
 
     def disjunction(self):
         # end run and begin new run, after which perform disjunction of substring
         return (c, '|')
 
-    def open_group(self, c):
+    def open_group(self):
         # begin series of disjunctions
-        substring_open = False
-        return (c, '[')
+        pass
 
     def close_group(self):
         # end series of disjunctions
@@ -66,10 +82,8 @@ class Sequence:
         substring_open = False
         return (c, ']')
 
-    def star(self, s1):
-        # next substring concatenates
-        substring_open = True
-        return (s1, '*')
+    def star(self):
+        return (self, '*')
 
     def literal(self, c):
         return (c, 'lit')
@@ -84,8 +98,11 @@ class Sequence:
         pass
 
     def concatenate(self):
-        concatenated = (self.out_seq[0])
-        for item in self.out_seq[1:]:
-            concatenated = (concatenated, item, '_')
-        return concatenated
+        if isinstance(self.out_seq, tuple):
+            return self.out_seq
+        else:
+            concatenated = (self.out_seq[0])
+            for item in self.out_seq[1:]:
+                concatenated = (concatenated, item, '_')
+            return concatenated
 
